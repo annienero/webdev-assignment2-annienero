d04d6dc8fd96b3545d696965533df0d97c964e3c
Annie <annienero93@gmail.com>
2018-07-12 11:18:51 -0400

    create course list

---
+++ src/containers/CourseList.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class CourseList extends Component {
+    render() {
+        return (
+            <h1>Course List</h1>
+        )
+    }  
+}
+
+export default CourseList;
