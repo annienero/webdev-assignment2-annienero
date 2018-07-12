b587c249db4e83bea7c2efcbb832d7530d57fdb0
Annie <annienero93@gmail.com>
2018-07-12 11:19:58 -0400

    create lesson tabs

---
+++ src/containers/LessonTabs.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class LessonTabs extends Component {
+    render() {
+        return (
+            <h1>Lessons</h1>
+        )
+    }  
+}
+
+export default LessonTabs;
