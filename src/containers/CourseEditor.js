109fd98a1e288647bb04c9578e61ac2b54e242c7
Annie <annienero93@gmail.com>
2018-07-12 11:18:26 -0400

    create course editor

---
+++ src/containers/CourseEditor.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class CourseEditor extends Component {
+    render() {
+        return (
+            <h1>Course Editor</h1>
+        )
+    }  
+}
+
+export default CourseEditor;
