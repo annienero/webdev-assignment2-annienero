5c2c737f9796ba82517071dd65f4165ecbab90af
Annie <annienero93@gmail.com>
2018-07-16 12:52:52 -0400

    move course row to own file

--- src/containers/CourseManager.js
+++ src/containers/CourseManager.js
@@ -1,21 +1,7 @@
 import React, {Component} from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
-
-class CourseCard extends Component {
-    render() {
-        return(
-            <div className="card" styles={{width: '18rem'}}>
-                <img className="card-img-top"
-                    src="https://picsum.photos/300/200"/>
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Card text.</p>
-                    <a href="#" className="btn btn-primary">More...</a>
-                </div>
-            </div>)
-    }
-}
+import CourseRow from './components/CourseRow'
 
 export default class CourseManager extends Component {
     render() {
@@ -23,43 +9,10 @@ export default class CourseManager extends Component {
             <div className="container-fluid">
                 <h1>Course Manager</h1>
                 <div className="card-deck">
-                    <CourseCard/>
-                    <CourseCard/>
+                    <CourseRow/>
+                    <CourseRow/>
                 </div>
             </div>
         )
     }  
 }
-
-export default class ModuleListItem extends React.Component {
-    render() {
-        return (
-            <li className="list-group-item">
-                {this.props.title} // for dynamic setting
-                <i className="fa fa-trash"></i>
-                <i className="fa fa-pencil"></i>
-            </li>
-        );
-    }
-}
-
-export default class ModuleList extends React.Component {
-    render() { 
-        return (
-            <ul className="list-group">
-                <ModuleListItem/>
-                <ModuleListItem/>
-            </ul>
-        );
-    }
-}
-export default class LessonTabs extends React.Component {
-    render() { 
-        return(
-            <ul class="nav nav-tabs">
-                <li class="nav-item"><a class="nav-link active"
-                    href="#">Active Tab</a></li>
-                <li class="nav-item"><a class="nav-link"
-                    href="#">Another Tab</a></li>
-            </ul>
-);}}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CourseEditor from './containers/CourseManager'
+import CourseManager from './containers/CourseManager'
 
 ReactDOM.render(
     <CourseManager/>,
