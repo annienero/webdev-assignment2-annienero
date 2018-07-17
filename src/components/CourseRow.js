5f4d98710a128c35239178424592862fa96caa4e
Annie <annienero93@gmail.com>
2018-07-16 20:09:13 -0400

    update course row titles dynamically

--- src/components/CourseRow.js
+++ src/components/CourseRow.js
@@ -7,8 +7,8 @@ export default class CourseRow extends Component {
                 <img className="card-img-top"
                     src="https://picsum.photos/300/200"/>
                 <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Card text.</p>
+                    <h5>{this.props.title}</h5>
+                    <p>{this.props.description}</p>
                     <a href="#" className="btn btn-primary">More...</a>
                 </div>
             </div>)
