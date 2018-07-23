1c45bbf633e9bd891f28dcb7f9e1c314e6be5632
Annie <annienero93@gmail.com>
2018-07-23 17:33:18 -0400

    set widget types

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import {DELETE_WIDGET} from '../constants/WidgetConstants'
+import {DELETE_WIDGET, SELECT_WIDGET_TYPE} from '../constants/WidgetConstants'
 
 import {connect} from 'react-redux';
 
-const Widget = ({widget, dispatch}) => (
-    <li>{widget.text}
-        <button onClick={e => (
-            dispatch({type: DELETE_WIDGET, id: widget.id})
-        )}>Delete</button>
-    </li>
-)
+const Widget = ({widget, dispatch}) => {
+    let selectElement
+    return(
+        <li>{widget.className}
+            <select defaultValue={widget.className} onChange={e => dispatch({type: SELECT_WIDGET_TYPE, id: widget.id, className: selectElement.value})} ref={node => selectElement = node}>
+                <option>Heading</option>
+                <option>Image</option>
+                <option>Link</option>
+                <option>List</option>
+                <option>Paragraph</option>
+            </select>
+            <button onClick={e => (
+                dispatch({type: DELETE_WIDGET, id: widget.id})
+            )}>Delete</button>
+        </li>
+    )
+}
 
 export const WidgetContainer = connect()(Widget)
--- src/reducers/WidgetReducer.js
+++ src/reducers/WidgetReducer.js
@@ -1,8 +1,18 @@
-import {ADD_WIDGET, DELETE_WIDGET, FIND_ALL_WIDGETS, SAVE_WIDGETS} from '../constants/WidgetConstants'
+import {ADD_WIDGET, DELETE_WIDGET, FIND_ALL_WIDGETS, SAVE_WIDGETS, SELECT_WIDGET_TYPE} from '../constants/WidgetConstants'
 
 let autoIncrement = 0
 export const WidgetReducer = (state = {widgets: []}, action) => {
     switch(action.type) {
+        case SELECT_WIDGET_TYPE:
+            let newState = {
+                widgets: state.widgets.filter((widget) => {
+                    if (widget.id === action.id) {
+                        widget.className = action.className
+                    }
+                    return true;
+                })
+            }
+            return JSON.parse(JSON.stringify(newState))
         case SAVE_WIDGETS:
             fetch('http://localhost:8080/api/lesson/62/widget/save', { //TODO eventually ACTUALLY do this for each lesson
                 method: 'post',
@@ -16,7 +26,7 @@ export const WidgetReducer = (state = {widgets: []}, action) => {
             return {
                 widgets: [
                     ...state.widgets,
-                    {text: 'New Widget', id: autoIncrement++}
+                    {text: 'New Widget', id: autoIncrement++, className: 'Paragraph'}
                 ]
             }
         case DELETE_WIDGET:
